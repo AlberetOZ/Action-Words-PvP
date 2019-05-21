6ea1b1cf0204d3cea3218115577f854f590c334c
AlberetOZ <32402386+AlberetOZ@users.noreply.github.com>
2019-05-21 12:36:18 +0300

    Final serv

--- Server  2/src/Server.java
+++ Server  2/src/Server.java
@@ -1,59 +1,193 @@
 import java.io.*;
 import java.net.*;
 import java.util.LinkedList;
-
-
+import java.util.concurrent.ThreadLocalRandom;
 
 class ServerSomthing extends Thread {
 
     private Socket socket;
     private InputStreamReader in;
     private OutputStreamWriter out;
+    public OutputStreamWriter out_2;
 
     public ServerSomthing(Socket socket) throws IOException {
         this.socket = socket;
         in = new InputStreamReader(socket.getInputStream());
         out = new OutputStreamWriter(socket.getOutputStream());
-       // сооюбщений новому поключению
+        Server.story.addStoryEl("$$$$$$");
         start(); // вызываем run()
     }
     @Override
     public void run() {
         String word;
         char[] wor = new char[256];
+        char[]w = new char[256];
         System.out.println("New _ Player");
         try {
-            int i = 0;
             int j = 0;
-            // первое сообщение отправленное сюда - это никнейм
             //word = in.readLine();
             try {
                 while (true) {
-                    in.read(wor);
-                    word = new String(wor);
-
-                    while(Server.serverList.size() < 2){
-                        i = i+1;
-                        if(i % 100000 == 56)
-                            Server.serverList.get(0).send("0");
-                    }
-                    if(j == 0){
-                        Server.serverList.get(1).send("1");
-                        Server.serverList.get(0).send("1");
-                        j++;
-                    }
-                    if(wor[0] == 1){
-                        j = 2;
-
-                    }
-                    System.out.println("Echoing: " + word);
-                    Server.story.addStoryEl(word);
-                    //for (ServerSomthing vr : Server.serverList) {
-                    if((socket != Server.serverList.getLast().socket))
-                        Server.serverList.get(1).send(word);
-                    else
-                        Server.serverList.get(0).send(word);// отослать принятое сообщение с привязанного клиента всем остальным влючая его
+                    //Server.serverList.get(0).send("lolicon4ik228");
+                    //wor = new char[2];
+                    //wor[0] = '1';
+                    //wor[1] = '9';
+                    //out.write(wor);
+                    //if(){
+                    //    socket.close();
                     //}
+
+
+                    out.flush();
+                    if(j == 0 && Server.serverList.size() == 1) {
+                        continue;
+
+                    }else{
+
+                        if(j == 0 && Server.serverList.size() == 2){
+                            if ((socket != Server.serverList.getLast().socket))
+                                Server.serverList.get(1).send("1");
+                            else
+                                Server.serverList.get(0).send("1");// отосла
+                            System.out.print("1 to second? - ");
+                            System.out.println(!(socket == Server.serverList.getLast().socket));
+
+                            if ((socket != Server.serverList.getLast().socket)) {
+                                out_2 = new OutputStreamWriter(Server.serverList.get(1).socket.getOutputStream());
+
+                            }
+                            else {
+                                out_2 = new OutputStreamWriter(Server.serverList.get(0).socket.getOutputStream());
+                            }
+
+
+
+                            j = j+1;
+                        }else
+                            wor = new char[256];
+                            int bytesReceived = in.read(wor);
+
+                        System.out.print("Received bytes are converted to ");
+                        System.out.println(new String(wor));
+                        System.out.println(bytesReceived);
+                            //word = word.split();
+                        char[] bytes = new char[bytesReceived];
+                        for (int i = 0; i < bytesReceived; i++)
+                        {
+                            bytes[i] = (char)wor[i];
+                        }
+                            wor = w;
+                            word = new String(bytes);
+                            wor = bytes;
+                            //word = new String(wor);
+                            if(wor[0] == '\0'){
+                                continue;
+                            }
+                            if(wor.length > 1 && wor[1] != '\0') {
+                                System.out.println("WOOOORD");
+                                System.out.println(word.length());
+                                //System.out.println(word);
+                                //System.out.println(Server.story.story.getLast());
+                                if (word.equalsIgnoreCase(Server.story.story.getLast())) {
+
+                                    System.out.println("CHEEEEATER");
+                                    downService();
+                                    break;
+                                }
+                            }
+                            Server.story.addStoryEl(word);
+                    }
+                    //}
+
+                    System.out.println("REAAAAAD");
+
+                    //while(Server.serverList.size() == 1){
+                   //     i = i+1;
+                    //}
+/*
+                    while(Server.serverList.size() == 1){
+                        i = i+1;
+                        if(i % 1000000 == 56)
+                            Server.serverList.get(0).send("0");
+                        word ="";
+
+                        //System.out.println("0");
+                    }
+*/
+
+                    if(j == 0 && Server.serverList.size() == 2){
+                        if ((socket != Server.serverList.getLast().socket)) {
+                            Server.serverList.get(1).send("1");
+                            System.out.print("1 to second");
+                        }
+                        else {
+                            Server.serverList.get(0).send("1");// отосла
+                            System.out.print("1 to first");
+
+                        }
+                        //System.out.println(!(socket == Server.serverList.getLast().socket));
+                        j = j+1;
+                    } else
+                    if('1' == wor[0]){
+
+                        if((socket == Server.serverList.getLast().socket)) {
+                            Server.serverList.get(0).send("1");
+
+                            System.out.print("1 send to 1");
+
+                            //Server.serverList.get(1).send("0");
+                        }
+                        else{
+                            //Server.serverList.get(0).send("0");
+                            System.out.print("2 send to 2");
+
+                            Server.serverList.get(1).send("2");
+                        }
+
+
+                        //System.out.println(!(socket == Server.serverList.getLast().socket));
+                        word = "";
+
+                    }else
+                        if('$' == wor[0]){
+
+                            if((socket == Server.serverList.getLast().socket)) {
+                                //Server.serverList.get(0).send("2");
+                                Server.serverList.get(0).send("1");
+                                System.out.print("1 send to 1");
+
+                            }
+                            else{
+                                Server.serverList.get(1).send("2");
+                                System.out.print("2 send to 2");
+
+                                //Server.serverList.get(1).send("2");
+                            }
+                        }
+                        else{
+
+                            System.out.println("You make this:    "+word);
+
+                        //for (ServerSomthing vr : Server.serverList) {
+                        //if ((socket != Server.serverList.getLast().socket)) {
+                            //Server.serverList.get(1).send(word);
+                            //OutputStreamWriter out_2 = new OutputStreamWriter(Server.serverList.get(1).socket.getOutputStream());
+                            //out = new OutputStreamWriter(());
+                            out_2.write(word);
+                            out_2.flush();
+                            //out_2.flush();
+                        //}
+                        //else{
+                            //Server.serverList.get(0).send(word);// отослать принятое сообщение с привязанного клиента всем остальным влючая его
+
+                            //OutputStreamWriter out_2 = new OutputStreamWriter(Server.serverList.get(0).socket.getOutputStream());
+                            //out = new OutputStreamWriter(());
+                            //out_2.write(wor);
+
+                            //out_2.flush();
+                        //}
+
+                        }
                 }
             } catch (NullPointerException ignored) {}
 
@@ -93,7 +227,7 @@ class ServerSomthing extends Thread {
 
 class Story {
 
-    private LinkedList<String> story = new LinkedList<>();
+    public LinkedList<String> story = new LinkedList<>();
 
     /**
      * добавить новый элемент в список
@@ -142,7 +276,17 @@ public class Server {
                 Socket socket = server.accept();
 
                 try {
-                    serverList.add(new ServerSomthing(socket));
+/*
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+                    //serverList.add(new ServerSomthing(socket));
+                    if(randomNum == 0){
+                        serverList.addFirst(new ServerSomthing(socket));
+                    }else{
+                        serverList.addLast(new ServerSomthing(socket));
+                    }
+*/
+
+                    serverList.addLast(new ServerSomthing(socket));
                     // добавить новое соединенние в список
                 } catch (IOException e) {
                     // Если завершится неудачей, закрывается сокет,
